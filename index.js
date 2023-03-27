1ee6662c6ebfe98c9a72bff22de737a2aa7c8c49
Ethan Beneroff <ethanbeneroff@gmail.com>
2023-03-27 18:09:48 -0400

    creates and inserts dom nodes with js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const element = document.getElementById("main");
+main.remove();
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory")
+newHeader.textContent = "ethan is the champion"
